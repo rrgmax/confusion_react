94bd21cc2d44fea5699016929ce8221e38d6bc88
Rogerio <rrgmax@bol.com.br>
2019-03-21 08:47:21 -0300

    Header and Footer Fixed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 
 class Main extends Component {
@@ -26,10 +26,6 @@ class Main extends Component {
         <DishDetail 
             dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
         <Footer />
-        <Menu dishes={this.state.dishes}
-            onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail 
-            dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish )[0]} />
       </div>
     );
   }
